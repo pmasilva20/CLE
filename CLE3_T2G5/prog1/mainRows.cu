587dd551cacb8e0d434b8d4c21a80ee08ef1be76
JoaoTS20 <joaots@ua.pt>
2022-06-25 01:40:11 +0100

    Rows

--- CLE3_T2G5/prog1/mainRows.cu
+++ CLE3_T2G5/prog1/mainRows.cu
@@ -27,8 +27,11 @@ static void modify_sector_cpu_kernel (unsigned int *sector_data, unsigned int se
                                       unsigned int sector_size);
 __global__ static void modify_sector_cuda_kernel (unsigned int * __restrict__ sector_data, unsigned int * __restrict__ sector_number,
                                                   unsigned int n_sectors, unsigned int sector_size);
-static double get_delta_time(void);
 
+__global__  static void updateMatrixCols(double* d_matrices, double* d_results);
+
+static double get_delta_time(void);
+void printResults(int numberOfMatrices, double * resultsDeterminant);
 /**
  *   main program
  */
@@ -136,69 +139,47 @@ int main (int argc, char **argv)
 
   n_sectors = N_SECTORS;
   sector_size = SECTOR_SIZE;
-  blockDimX = 1 << 0;                                      // optimize!
+  blockDimX = orderOfMatrices;                             // optimize!
   blockDimY = 1 << 0;                                      // optimize!
   blockDimZ = 1 << 0;                                      // do not change!
-  gridDimX = 1 << 21;                                      // optimize!
+  gridDimX = numberOfMatrices;                             // optimize!
   gridDimY = 1 << 0;                                       // optimize!
   gridDimZ = 1 << 0;                                       // do not change!
 
   dim3 grid (gridDimX, gridDimY, gridDimZ);
   dim3 block (blockDimX, blockDimY, blockDimZ);
 
-  if ((gridDimX * gridDimY * gridDimZ * blockDimX * blockDimY * blockDimZ) != n_sectors)
+  /**if ((gridDimX * gridDimY * gridDimZ * blockDimX * blockDimY * blockDimZ) != n_sectors)
      { printf ("Wrong configuration!\n");
        return 1;
      }
+  **/
   (void) get_delta_time ();
-  modify_sector_cuda_kernel <<<grid, block>>> (device_sector_data, device_sector_number, n_sectors, sector_size);
+  updateMatrixCols <<<grid, block>>> (matricesDevice,resultsDeterminantDevice);
   CHECK (cudaDeviceSynchronize ());                            // wait for kernel to finish
   CHECK (cudaGetLastError ());                                 // check for kernel errors
   printf("The CUDA kernel <<<(%d,%d,%d), (%d,%d,%d)>>> took %.3e seconds to run\n",
          gridDimX, gridDimY, gridDimZ, blockDimX, blockDimY, blockDimZ, get_delta_time ());
 
-  /* copy kernel result back to host side */
+  /* Copy kernel result back to host side */
 
-  unsigned int *modified_device_sector_data;
+  /** Determinant Results from Device/GPU **/
+  double *resultsDeterminantFromDevice = (double *)malloc(sizeof(double) * numberOfMatrices);
 
-  modified_device_sector_data = (unsigned int *) malloc (sector_data_size);
-  CHECK (cudaMemcpy (modified_device_sector_data, device_sector_data, sector_data_size, cudaMemcpyDeviceToHost));
-  printf ("The transfer of %ld bytes from the device to the host took %.3e seconds\n",
-          (long) sector_data_size, get_delta_time ());
+  CHECK(cudaMemcpy(resultsDeterminantFromDevice, resultsDeterminantDevice, numberOfMatrices * sizeof(double), cudaMemcpyDeviceToHost));
+  printf ("The transfer of Determinant Results from the device to the host took %.3e seconds\n", get_delta_time ());
 
-  /* free device global memory */
+  /** Free Matrices from Device/GPU  Global Memory **/
+  CHECK (cudaFree (matricesDevice));
 
-  CHECK (cudaFree (device_sector_data));
-  CHECK (cudaFree (device_sector_number));
+  /** Free Results Determinant Device/GPU Global Memory **/
+  CHECK (cudaFree (resultsDeterminantDevice));
 
-  /* reset the device */
-
+  /* Reset the Device / GPU */
   CHECK (cudaDeviceReset ());
 
-  /* compute the modified sector data on the CPU */
-
-  (void) get_delta_time ();
-  for (i = 0; i < N_SECTORS; i++)
-    modify_sector_cpu_kernel (&host_sector_data[i*SECTOR_SIZE/(sizeof (unsigned int))], host_sector_number[i], n_sectors, sector_size);
-  printf("The cpu kernel took %.3e seconds to run (single core)\n",get_delta_time ());
-
-  /* compare results */
-
-  for(i = 0; i < (int) sector_data_size / (int) sizeof (unsigned int); i++)
-    if (host_sector_data[i] != modified_device_sector_data[i])
-       { int sector_words = sector_size / (int) sizeof (unsigned int);
-
-         printf ("Mismatch in sector %d, word %d\n", i / sector_words, i % sector_words);
-         exit(1);
-       }
-  printf ("All is well!\n");
-
-  /* free host memory */
-
-  free (host_sector_data);
-  free (host_sector_number);
-  free (modified_device_sector_data);
-
+  printResults(numberOfMatrices,resultsDeterminantFromDevice);
+  
   return 0; 
 }
 
@@ -227,54 +208,40 @@ static void modify_sector_cpu_kernel (unsigned int *sector_data, unsigned int se
   }
 }
 
-__global__ static void modify_sector_cuda_kernel (unsigned int * __restrict__ sector_data, unsigned int * __restrict__ sector_number,
-                                           unsigned int n_sectors, unsigned int sector_size)
-{
-  unsigned int x, y, idx, i, a, c, n_words;
+//TODO: Colocar aqui versão CPU
 
-  /* compute the thread number */
+__global__  static void updateMatrixCols(double* d_matrices, double* d_results) {
+	int n = blockDim.x;
+	for (int iter = 0; iter < n; iter++) {
+   
+    if (threadIdx.x < iter) 
+      continue;
 
-  x = (unsigned int) threadIdx.x + (unsigned int) blockDim.x * (unsigned int) blockIdx.x;
-  y = (unsigned int) threadIdx.y + (unsigned int) blockDim.y * (unsigned int) blockIdx.y;
-  idx = (unsigned int) blockDim.x * (unsigned int) gridDim.x * y + x;
-  if (idx >= n_sectors)
-     return;                                             // safety precaution
+    int matrixId = blockIdx.x * n * n;
+    int col = matrixId + threadIdx.x;			// current col offset of this (block thread)	
+    int iterCol = matrixId + iter;
 
-  /* convert the sector size into number of 4-byte words (it is assumed that sizeof(unsigned int) = 4)
-     and define boundaries */
+    if (threadIdx.x == iter) {
+      if (iter == 0)
+        d_results[blockIdx.x] = 1;
+      d_results[blockIdx.x] *= d_matrices[iterCol + iter * n];
+      continue;
+    }
 
-  n_words = sector_size / 4u;
+    double pivot = d_matrices[iterCol + iter * n];
 
-  /* adjust pointers */
-
-  sector_data += n_words * idx;
-  sector_number += idx;
-
-  /* initialize the linear congruencial pseudo-random number generator
-     (section 3.2.1.2 of The Art of Computer Programming presents the theory behind the restrictions on a and c) */
-
-  i = sector_number[0];                                    // get the sector number
-  a = 0xCCE00001u ^ ((i & 0x0F0F0F0Fu) << 2);              // a must be a multiple of 4 plus 1
-  c = 0x00CCE001u ^ ((i & 0xF0F0F0F0u) >> 3);              // c must be odd
-  x = 0xCCE02021u;                                         // initial state
-
-  /* modify the sector data */
-
-  for (i = 0u; i < n_words; i++)
-  { x = a * x + c;                                          // update the pseudo-random generator state
-    sector_data[i] ^= x;
+    double value = d_matrices[col + iter * n] / pivot;
+    for (int i = iter + 1; i < n; i++) {
+      d_matrices[col + i * n] -= d_matrices[iterCol + i * n] * value; 
+    }
+    __syncthreads();
   }
 }
 
-static double get_delta_time(void)
-{
-  static struct timespec t0,t1;
-
-  t0 = t1;
-  if(clock_gettime(CLOCK_MONOTONIC,&t1) != 0)
-  {
-    perror("clock_gettime");
-    exit(1);
+void printResults(int numberOfMatrices, double *resultsDeterminant){
+  
+  for (int a = 0; a < numberOfMatrices; a++) {
+      printf("Matrix %d :\n", a + 1);
+      printf("The determinant is %.3e\n", resultsDeterminant[a]);
   }
-  return (double)(t1.tv_sec - t0.tv_sec) + 1.0e-9 * (double)(t1.tv_nsec - t0.tv_nsec);
 }
